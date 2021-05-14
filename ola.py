cdc5fa84c4d93a425c89b9f03c921a1e205f97bb
Franco Alvarez <francoalvarez@MacBook-Pro-de-Franco.local>
2021-05-13 23:57:57 -0400

    nuevo commit de prueba

--- ola.py
+++ ola.py
@@ -5,3 +5,6 @@ data = pd.DataFrame(pd.read_excel('onco_data1_2.xlsx',sheet_name='datos_onco1'))
 subset = data[['RECORD_ID']]
 
 np.savetxt(r'datos.txt', subset.values, fmt='%s')
+
+##cambios que deberian aparecer con el nuevo push
+
